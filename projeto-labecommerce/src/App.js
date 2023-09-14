0feec32363dc7a4c8f67dded45df38137e063f9b
LCassio99 <lucianocassio66@gmail.com>
2023-09-13 23:25:17 -0300

    [configuração] criei os componentes principais do projeto

--- projeto-labecommerce/src/App.js
+++ projeto-labecommerce/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Filters from './Components/Filters/Filters.js';
+import Body from './Components/ProductList/Home/Body.js';
+import Footer from './Components/ProductList/Home/Footer.js';
+import Header from './Components/ProductList/Home/Header.js';
+import Navigation from './Components/ProductList/Home/Navigation.js';
+import Cart from './Components/ShoppingCart/Cart/Cart.js';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Filters />
+      <Body />
+      <Footer />
+      <Header />
+      <Navigation />
+      <Cart />
     </div>
   );
 }
-
-export default App;
